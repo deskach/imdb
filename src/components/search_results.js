694c26c36b83743bee9d73d5510d2a441ec74c5b
deskach <dennis.kachanau@rexsoftware.com.au>
2017-01-09 16:07:19 +1000

    Trying to use react-refetch

--- src/components/search_results.js
+++ src/components/search_results.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect, PromiseState } from 'react-refetch'
 
-const VideoListItem = ({video, onVideoSelect}) => {
-  return (
-      <div>Search results</div>
-  );
-};
+class SearchResults extends Component {
+  static propTypes = {
+    term: PropTypes.string.isRequired,
+    movieFetch: PropTypes.instanceOf(PromiseState).isRequired,
+  };
 
-export default VideoListItem;
+  render () {
+    const { movieFetch } = this.props;
+
+    if (movieFetch.pending) {
+      return <div>Loading...</div>;
+    } else if (movieFetch.rejected) {
+      return <div> Failure </div>;
+    } else if (movieFetch.fulfilled) {
+      return <div>Success</div>
+    }
+  }
+}
+
+export default connect(props => {
+  return {
+    movieFetch: {
+      url: `http://www.omdbapi.com/?s=${props.term}&type=movie`,
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+    },
+  }
+})(SearchResults)
 
